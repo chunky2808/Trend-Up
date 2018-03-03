12645c6ae667ff4728c53bdd341602ded159c0e8
chunky2808 <parasjain2896@gmail.com>
2018-03-03 22:52:54 +0530

    Final

--- twitter bot.py
+++ twitter bot.py
@@ -1,30 +1,35 @@
 import tweepy
 from time import sleep
-  
+
+#Oauth required for using twitter API(enter your fields in between '')
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
+#Oauth required for using twitter API(enter your fields in between '')
 
-
+#Oauth verification
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+#Oauth verification
 
 print("Chose option 1) For Just Copying Tweet and automating printing it from your account.")
-print("Chose option 2) For Retweeting,following,mark tweet as favourite")
+print("Chose option 2) For Just Copying Tweet from accounts which you are following and automating printing it from your account")
+print("Chose option 3) For Retweeting,following,mark tweet as favourite")
 
 a = input()
 
 
+#class for stream listener
 class StdOutListener(tweepy.StreamListener):
     
     def on_status(self, status):
-        print(('Tweet text: ' + status.text).encode('utf-8'))
+        print(('Tweet text: ' + status.text).encode('utf-8'))#encoding
         api = tweepy.API(auth)
         try:
             if status.text != '\n':
-                api.update_status(status.text)
+                api.update_status(status.text)#update data to twitter account
             else:
                 pass
         except tweepy.TweepError as e:
@@ -38,17 +43,29 @@ class StdOutListener(tweepy.StreamListener):
     def on_timeout(self):
         print('Timeout...')
         return True
+#class for stream listener
 
-
-
+#For Just Copying Tweet and automating printing it from your account.
 if a==1 :
   listener = StdOutListener()
   stream = tweepy.Stream(auth, listener)
   #follow_id =  ['967302676483657728']
   track_tag = ['#TripuraElection2018']
   stream.filter(track = track_tag)
-  #stream.filter(follow = ['967302676483657728']   
+  #stream.filter(follow = ['967302676483657728']  
+#For Just Copying Tweet and automating printing it from your account.
 
+#For Just Copying Tweet from accounts which you are following and automating printing it from your account
+elif a==2 :
+  listener = StdOutListener()
+    stream = tweepy.Stream(auth, listener)
+    follow_id =  ['967302676483657728']
+    #track_tag = ['#TripuraElection2018']
+    #stream.filter(track = track_tag)
+    stream.filter(follow = ['967302676483657728'])
+#For Just Copying Tweet from accounts which you are following and automating printing it from your account"
+
+#For Retweeting,following,mark tweet as favourite
 else:
   for tweet in tweepy.Cursor(api.search, q='#WhySalaryStopped',since='2018-03-02',until='2018-03-04').items():
       try:
@@ -61,3 +78,4 @@ else:
 
       except StopIteration:
           break
+#For Retweeting,following,mark tweet as favourite
