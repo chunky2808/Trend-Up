7bd1807a05126004100e5e0acfaaec0ba6cd3386
chunky2808 <parasjain2896@gmail.com>
2018-03-03 22:40:20 +0530

    Added more options

--- twitter bot.py
+++ twitter bot.py
@@ -1,12 +1,22 @@
 import tweepy
-import sys
 from time import sleep
-
+  
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
- 
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+print("Chose option 1) For Just Copying Tweet and automating printing it from your account.")
+print("Chose option 2) For Retweeting,following,mark tweet as favourite")
+
+a = input()
+
+
 class StdOutListener(tweepy.StreamListener):
     
     def on_status(self, status):
@@ -29,12 +39,25 @@ class StdOutListener(tweepy.StreamListener):
         print('Timeout...')
         return True
 
-if __name__ == '__main__':
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    listener = StdOutListener()
-    stream = tweepy.Stream(auth, listener)
-    #follow_id =  ['967302676483657728']
-    track_tag = ['#TripuraElection2018']
-    stream.filter(track = track_tag)
-    #stream.filter(follow = ['967302676483657728']
+
+
+if a==1 :
+  listener = StdOutListener()
+  stream = tweepy.Stream(auth, listener)
+  #follow_id =  ['967302676483657728']
+  track_tag = ['#TripuraElection2018']
+  stream.filter(track = track_tag)
+  #stream.filter(follow = ['967302676483657728']   
+
+else:
+  for tweet in tweepy.Cursor(api.search, q='#WhySalaryStopped',since='2018-03-02',until='2018-03-04').items():
+      try:
+         tweet.retweet()
+         tweet.favorite()
+         tweet.user.follow()
+         
+      except tweepy.TweepError as e:
+          print(e.reason)
+
+      except StopIteration:
+          break
